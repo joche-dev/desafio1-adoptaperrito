b54167cf35deb126e5920affa6843493f24d4f68
Jose Albornoz <prueba@outlook.cl>
2023-08-06 00:40:14 -0400

    Subiendo desafio

---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,27 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p className="my-4">
+          Explora nuestra galería de adopción de perros para encontrar a tu
+          compañero perfecto. Tenemos una variedad de perros con diferentes
+          personalidades y tamaños, todos en busca de hogar amoroso. Cada imagen
+          captura su esencia única. Adopta un perro y bríndale una segunda
+          oportunidad. Encuentra a tu compañero peludo para siempre.
+        </p>
+        <div class="footer-icon">
+          <a href="#"><i class="fa-brands fa-facebook"></i></a>
+          <a href="#"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+        </div>
+        <p>
+          ©2023 Todos los derechos reservados - Adopta Perrito
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
